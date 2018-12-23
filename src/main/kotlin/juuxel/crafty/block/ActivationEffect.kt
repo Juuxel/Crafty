ad20809a6a19ff46992e9008e77b03b3b31cd1c7
Juuxel <kasperi.kauppi@gmail.com>
2018-12-23 22:20:28 +0200

    Update ActivationEffect.kt

--- src/main/kotlin/juuxel/crafty/block/ActivationEffect.kt
+++ src/main/kotlin/juuxel/crafty/block/ActivationEffect.kt
@@ -1,5 +1,6 @@
 /* This file is a part of the Crafty project
  * by Juuxel, licensed under the MIT license.
+ * Full code and license: https://github.com/Juuxel/Crafty
  */
 package juuxel.crafty.block
 
